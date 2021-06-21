62409f2f473e7b178ebf11f031e62e307b9b180d
Abhijay Jain <abhijay007j@gmail.com>
2021-06-21 23:38:15 +0530

    Add files via upload

---
+++ Basic/OOPs/class.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace  std;
+
+class person
+{
+    char name[20];
+    int id;
+public:
+    void getdetails(){}
+};
+  
+int main()
+{
+   person p1; // p1 is a object 
+}
